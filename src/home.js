a699ae4f7bd724466d4af89d2242ff0c326bb931
KunJ99 <kunjeong99@naver.com>
2019-07-17 22:57:59 +0900

    added redux and router-flux

--- src/home.js
+++ src/home.js
@@ -3,50 +3,34 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 
-class Page extends React.Component {
+class Home extends React.Component {
   render() {
     return (
       <View style={styles.fill}>
         <View style={styles.header}>
           <Text style={styles.text}>Current scene: "{this.props.currentScene}"</Text>
-          <Text style={styles.text}>This is an arbitrary page.</Text>
+          <Text style={styles.text}>Welcome to the home scene.</Text>
+          <Text style={styles.data}>{this.props.data ? this.props.data : 'No data...'}</Text>
         </View>
-        <TouchableOpacity
-          style={styles.data}
-          onPress={() => {
-            this.props.action('This is your data: [1, 2, 3]');
-          }}
-        >
-          <Text style={styles.text}> Get data </Text>
-        </TouchableOpacity>
         <TouchableOpacity
           style={styles.button}
           onPress={() => {
-            Actions.home();
+            Actions.page();
           }}
         >
-          <Text style={styles.text}>
-            {' '}
-            {'<'}
-            -- Go to "home"{' '}
-          </Text>
+          <Text style={styles.text}> Go to "page" --> </Text>
         </TouchableOpacity>
       </View>
     );
   }
 }
 
-const action = data => ({
-  type: 'data',
-  payload: data,
-});
-
 const styles = {
   fill: {
     flex: 1,
   },
   header: {
-    flex: 4,
+    flex: 2,
     padding: 20,
   },
   text: {
@@ -55,13 +39,13 @@ const styles = {
     justifyContent: 'center',
   },
   data: {
-    flex: 1,
-    backgroundColor: 'cyan',
-    justifyContent: 'center',
+    marginTop: 100,
+    fontSize: 30,
+    textAlign: 'center',
   },
   button: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: 'green',
     justifyContent: 'center',
   },
 };
@@ -70,5 +54,5 @@ const mapStateToProps = ({ reducer }) => ({ data } = reducer);
 
 export default connect(
   mapStateToProps,
-  { action },
-)(Page);
+  {},
+)(Home);
